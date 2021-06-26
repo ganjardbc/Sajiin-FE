45382deb9d80a82f742d66ef8076d1ccd39895a2
Ganjar Hadiatna <ganjardbc@gmail.com>
2021-06-26 13:14:18 +0700

    update config

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Sajiin-UI/',
+    assetsPublicPath: '/Sajiin-FE/',
 
     /**
      * Source Maps
